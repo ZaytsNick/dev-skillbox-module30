f6653902f8ddcfc8c793f18b67223b61a867f0b6
zayts_home <10kcin01@gmail.com>
2024-06-03 22:57:01 +0500

    trying to include the library cpr: Code execution cannot continue because the system did not detect cpr.dll

---
+++ Task_1/Task_1.cpp
@@ -0,0 +1,24 @@
+﻿// Task_1.cpp: определяет точку входа для приложения.
+//
+#include <iostream>
+#include <cpr/cpr.h>
+
+
+int main()
+{
+	cpr::Response get = cpr::Get(cpr::Url("http://httpbin.org/"));
+	std::cout << get.text << std::endl;
+
+	cpr::Response post = cpr::Post(cpr::Url("http://httpbin.org/"));
+	std::cout<< post.text << std::endl;
+
+	cpr::Response put = cpr::Put(cpr::Url("http://httpbin.org/"));
+	std::cout << put.text << std::endl;
+
+	cpr::Response del = cpr::Delete(cpr::Url("http://httpbin.org/"));
+	std::cout << del.text << std::endl;
+
+	cpr::Response patch = cpr::Patch(cpr::Url("http://httpbin.org/"));
+	std::cout << patch.text << std::endl;
+}
+	
