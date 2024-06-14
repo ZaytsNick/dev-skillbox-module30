a7e9da4b4c74c907c8efc7e7bb6a244d87315b6f
zayts_home <10kcin01@gmail.com>
2024-06-14 22:17:44 +0500

    Task#2 complite

---
+++ Task_2/Task_2.cpp
@@ -0,0 +1,17 @@
+﻿// Task_2.cpp: определяет точку входа для приложения.
+//
+
+#include <iostream>
+#include "cpr/cpr.h"
+
+int main()
+{
+	cpr::Response get = cpr::Get(cpr::Url("http://httpbin.org/html"));
+	int start = get.text.find("<h1>");
+	int end = get.text.find("</h1>");
+	for (int i = start+4; i < end; i++)
+	{
+		std::cout << get.text[i];
+	}
+	return 0;
+}
